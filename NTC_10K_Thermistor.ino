b968ea1f491622fad4960395d8f87eb3aaf6b182
neilsroberts <neil.s.roberts@amphenol-sensors.com>
2021-01-05 12:43:33 +0000

    Temperature NTC Example
    Use of Steinhart Hart equation to generate temperature

---
+++ NTC_10K_Thermistor.ino
@@ -0,0 +1,23 @@
+double Temp;
+
+#include <math.h>
+
+double Thermistor(int RawADC) {
+//Connection 5V - Thermistor - AI - 10k - GND
+  Temp = log(10000.0 * ((1024.0 / RawADC - 1)));
+  Temp = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * Temp * Temp )) * Temp );
+  Temp = Temp - 273.15;            // Convert Kelvin to Celcius
+}
+
+void setup() {
+  Serial.begin(19200);
+}
+
+void loop() {
+  Thermistor(analogRead(2));
+  Serial.print(Temp); Serial.print(" °C "); // display Celcius
+
+  Temp = (Temp * 1.8) + 32.0; // Convert Celcius to Fahrenheit return Temp;
+  Serial.print(Temp); Serial.println(" °F"); // display Fahrenheit
+  delay(2000);
+}
